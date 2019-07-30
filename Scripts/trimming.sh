a244f2b8074ceb43864d488332a8f874722782b5
Mathieu Quinodoz <46816306+mquinodo@users.noreply.github.com>
2019-07-30 10:44:26 +0200

    Delete trimming.sh

--- Scripts/trimming.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# trimming terminal het on both sides of regions 
-
-awk -F"\t" 'BEGIN{OFS="\t"} {if(state=="No" && $13=="Yes" && $6=="het") $13="No"; print $0} {state=$13} {zyg=$6}' $1 \
-| tac | awk -F"\t" 'BEGIN{OFS="\t"} {if(state=="No" && $13=="Yes" && $6=="het") $13="No"; print $0} {state=$13} {zyg=$6}' | tac > $2
-
