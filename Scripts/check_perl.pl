e3ea207bcd84d5dca2f640f842a3eeea3efec44d
Mathieu Quinodoz <46816306+mquinodo@users.noreply.github.com>
2019-07-30 10:43:42 +0200

    Delete check_perl.pl

--- Scripts/check_perl.pl
+++
@@ -1,9 +0,0 @@
-
-#!/usr/bin/perl
-use ExtUtils::Installed;
-my $InstalledModules = ExtUtils::Installed->new(); 
-foreach my $module ($InstalledModules->modules()) {
-	my $version = $InstalledModules->version($module) || "???";
-	print "$module — $versionn" . "\n";
-}
-
